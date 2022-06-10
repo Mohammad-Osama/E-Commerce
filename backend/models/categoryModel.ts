8e8d46ecbedbc819565dacde81492b699d4d9edb
Mohammad <moh.osama84x@gmail.com>
2022-06-10 21:43:30 +0200

    B - category model

--- backend/models/categoryModel.ts
+++ backend/models/categoryModel.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 
-export interface IBrand {
+export interface ICategory {
     name: string;
     description: string;
     logo:string
 }
 
-export const Brand = new mongoose.Schema<IBrand>({
+export const Brand = new mongoose.Schema<ICategory>({
     name: { type: String, required: [true, "Please add a name"] },
     description: { type: String, required: [true, "Please add a description"] },
     logo:{ type: String, required: [true, "Please add a logo"] },
