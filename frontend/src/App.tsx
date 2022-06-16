af90c34cebe9c05c765490adf36f0e455f6650cb
Mohammad <moh.osama84x@gmail.com>
2022-06-16 20:08:21 +0200

    F - initial navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,21 @@ import { Container } from '@mantine/core';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./user/components/pages/Home";
 import TempCart from "./user/components/TempCart";
+import { Navbar } from "./user/components/features/NavBar";
 
 
 const App: React.FC = () => {
   return (
+    <>
+    <Navbar/>
     <Container size="xl" px="xs" >
+     
       <TempCart />
       <Routes>
         <Route  path='/' element={<Home/>} />
       </Routes>
     </Container>
+    </>
   );
 }
 
---
+++ frontend/src/user/components/features/NavBar.tsx
@@ -0,0 +1,84 @@
+import React  from 'react'
+import { createStyles, Header, ActionIcon, Group, Burger, Container,Text } from '@mantine/core';
+import { useBooleanToggle } from '@mantine/hooks';
+import { ShoppingCart } from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useState , useEffect , useRef} from 'react';
+
+
+const useStyles = createStyles((theme) => ({
+    search: {
+        border: 'none',
+    },
+    inner: {
+        height: 56,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    links: {
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    burger: {
+        [theme.fn.largerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    link: {
+        display: 'block',
+        lineHeight: 1,
+        padding: '8px 12px',
+        borderRadius: theme.radius.sm,
+        textDecoration: 'none',
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+
+        '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+    },
+
+    linkLabel: {
+        marginRight: 5,
+    },
+}));
+
+
+
+export function Navbar() {
+    const [opened, toggleOpened] = useBooleanToggle(false);
+    const { classes } = useStyles();
+ 
+    return (
+        <Header height={56} mb={12}>
+            <Container>
+                <div className={classes.inner}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to="./">
+                        <h2>Home</h2>
+                    </Link>
+                   
+                    <Group spacing={5} className={classes.links} >
+                    <ActionIcon<typeof Link> component={Link} to="/cart" >
+                        <ShoppingCart />
+                    </ActionIcon>
+                    <Text > Cart </Text>
+                    </Group>
+                    <ShoppingCart />
+                    <Burger
+                        opened={opened}
+                        onClick={() => toggleOpened()}
+                        className={classes.burger}
+                        size="sm"
+                    />
+                </div>
+            </Container>
+        </Header>
+    );
+}
