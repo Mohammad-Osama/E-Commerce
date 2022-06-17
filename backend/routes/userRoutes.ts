05925c5de7d6e8b3b2bb12c195270cec2f7c6147
Mohammad <moh.osama84x@gmail.com>
2022-06-18 00:38:31 +0200

    B - user routes

---
+++ backend/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { registerUser } from "../controllers/userController"
+
+
+const router = express.Router()
+
+router.route("/register").post(registerUser)
+ // router.route("/login").post(loginUser)  to be added later 
+ // router.route("/:id").get(getUser).put(updateUser)  to be added later 
+
+
+
+module.exports = router
