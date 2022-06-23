5b4031fd89126ae50bb9c3d25aedc8ecfb29512a
Mohammad <moh.osama84x@gmail.com>
2022-06-23 02:05:37 +0200

    B - get products by category

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -1,84 +1,112 @@
-import {Request,Response} from "express"
-import { Product , IProduct } from "../models/productModel"
+import { Request, Response } from "express"
+import mongoose, { Schema } from "mongoose"
+import { Product, IProduct } from "../models/productModel"
 
 
 // all products with pages and limit
-export async function getProducts(req :Request , res : Response)  {
+export async function getProducts(req: Request, res: Response) {
+   try {
+      const page: number = parseInt(req.query.page as string) || 1
+      const limit: number = 12
+      const skip = (page - 1) * limit
 
-  const page : number = parseInt(req.query.page as string ) || 1
-  const limit : number = 12
-  const skip = (page-1)*limit
 
-  try {
-   const products  = await Product.find().skip(skip).limit(limit)
-  
-   res.status(200).json(products)
-   
-  } catch (error) {
-     res.status(400).json(`Error==>${error}`);
-  }
-  
- }
+      const products = await Product.find().skip(skip).limit(limit)
 
- // search products with name 
- export async function searchProducts(req :Request , res : Response)  {
-
-   const products  = await Product.find({"name": {"$regex": "sam" ,'$options' : 'i' },
-                                   }).limit(10)   
-  
       res.status(200).json(products)
+
+   } catch (error) {
+      res.status(400).json(`Error==>${error}`);
    }
 
- // new product 
-export async function addProduct(req :Request , res : Response)  {
-   
-    const newProduct : IProduct = await Product.create({
-      name: req.body.name ,
-      model: req.body.model,
-      main_image: req.body.main_image,
-      images: req.body.images,   
-      description: req.body.description,
-      price: req.body.price,
-      currency: req.body.currency,
-      stock: req.body.stock,
-      category: req.body.category,
-      brand: req.body.brand,
-      sale :req.body.sale,
-      featured :req.body.featured
-    })
+}
 
-   res.status(201).json(newProduct)
+// search products with name , query still to be written
+export async function searchProducts(req: Request, res: Response) {
+   try {
+      const products = await Product.find({
+         "name": { "$regex": "sam", '$options': 'i' },
+      }).limit(10)
+
+      res.status(200).json(products)
+
+   } catch (error) {
+      res.status(400).json(`Error==>${error}`);
+   }
+
+
+}
+
+//get products by category id 
+export async function getProductsCategory(req: Request, res: Response) {
+   try {
+      const queryID = req.query.category as string
+      const ObjectId = mongoose.Types.ObjectId
+      // example id  "62a5fd190044cc587c77ba29"
+      const finalID = new ObjectId(queryID);
+      const products = await Product.find({ category: finalID }).limit(3)
+
+      res.status(200).json(products)
+   } catch (error) {
+      res.status(400).json(`Error==>${error}`);
+   }
+
+}
+
+// new product 
+export async function addProduct(req: Request, res: Response) {
+   try {
+      const newProduct: IProduct = await Product.create({
+         name: req.body.name,
+         model: req.body.model,
+         main_image: req.body.main_image,
+         images: req.body.images,
+         description: req.body.description,
+         price: req.body.price,
+         currency: req.body.currency,
+         stock: req.body.stock,
+         category: req.body.category,
+         brand: req.body.brand,
+         sale: req.body.sale,
+         featured: req.body.featured
+      })
+
+      res.status(201).json(newProduct)
+   } catch (error) {
+      res.status(400).json(`Error==>${error}`);
+   }
+
 }
 
 // update  product 
-export async function updateProduct(req :Request , res : Response) {
-   const id : string = req.params.id
-   const existingProduct:IProduct | null = await Product.findById(id)
-   
-   if (!existingProduct){
+export async function updateProduct(req: Request, res: Response) {
+   const id: string = req.params.id
+   const existingProduct: IProduct | null = await Product.findById(id)
+
+   if (!existingProduct) {
       res.status(400)
-      throw new Error ("Product not found")
-     }         
+      throw new Error("Product not found")
+   }
    else {
       try {
-         const updatedProduct :IProduct | null = await Product.findByIdAndUpdate(
-            id ,req.body, {returnDocument:"after"}
-            )
-      
-          res.status(200).json(updatedProduct)
+         const updatedProduct: IProduct | null = await Product.findByIdAndUpdate(
+            id, req.body, { returnDocument: "after" }
+         )
+
+         res.status(200).json(updatedProduct)
       } catch (error) {
-         
+
          res.status(400).json(`Error==>${error}`);
       }
-      
-   }
-   
-}
- 
-// delete product 
-export function deleteProduct(req :Request , res : Response) :void {
 
-   res.status(200).json({message:`delete product with id ${req.params.id as unknown as number}`})
+   }
+
+}
+
+// delete product 
+export function deleteProduct(req: Request, res: Response): void {
+
+   res.status(200).json({ message: `delete product with id ${req.params.id as unknown as number}` })
 }
 
 
--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -3,7 +3,8 @@ import {getProducts ,
         addProduct , 
         updateProduct ,
         deleteProduct,   
-        searchProducts}
+        searchProducts,
+        getProductsCategory}
       from "../controllers/productController"
 
 
@@ -12,13 +13,15 @@ const router = express.Router()
 router.route("/")
                   .get(getProducts)
                   .post(addProduct)
-          
-
+  
 router.route("/:id")
                   .put(updateProduct)
                   .delete(deleteProduct)
 
 router.route("/search")
                   .get(searchProducts)
+
+router.route("/bycategory")
+                  .get(getProductsCategory)
 
 export = router
