b69ee0f83c0f6f86be8a7e2fa5d8121bd0b6eeea
Mohammad <moh.osama84x@gmail.com>
2022-07-12 00:43:40 +0200

    F - Profile shows data custom ordered,tsconfig es6

--- frontend/src/user/features/Profile.tsx
+++ frontend/src/user/features/Profile.tsx
@@ -4,69 +4,102 @@ import { authState } from '../redux/slices/authSlice';
 import { useSelector } from 'react-redux';
 import * as api from "../../helpers/api"
 import { IUser } from '../../helpers/types';
+import { Loader } from 'tabler-icons-react';
 const Profile = () => {
-
+    // not needed ?
     const { id, first_name, last_name, email } = useSelector(authState)
 
     const emptyUser = {} as IUser
 
     const [user, setUser] = useState<IUser>(emptyUser)
+    const [userMapped, setUserMapped] = useState<any>()
+    const [loading, setLoading] = useState(true)
 
     async function getUserData() {
         const resData = await api.getUserData()
+        const mapped = new Map<any, any>([
+            ["First Name", resData.first_name],
+            ["Last Name", resData.last_name],
+            ["Email", resData.email],
+            ["Birthday", resData.birthday],
+            ["Phone", resData.phone],
+            ["Address", resData.address],
+            ["Country", resData.country],
+        ])
+
+
+        let obj = []
+        for (const [key, value] of mapped) {
+            obj.push({ key, value });
+        }
         setUser(resData)
-       //  console.log("resDataaaa", resData)
+        setUserMapped(obj)
+        setLoading(false)
+
+
+
+
     }
 
     useEffect(() => {
         getUserData()
-    }, [])
+    }, [loading])
 
-    return (
-        <>
-            <Text align="center">
-                Profile
-            </Text>
 
-            <Divider />
-            <Divider />
-            <Divider />
-            <Grid columns={3} >  {/* coupon to be shown speratly from its document,to show the value not the id  */}
-                {Object.entries(user).filter(([key, _]) => key !== "id" && key!=="coupon").map(([key, value]) => {
-                    return (
-                        <React.Fragment key={key}>
+    if (loading === true)
+        return (
+            <Loader />
+        )
+    else
+
+        return (
+            <>
+
+                <Divider label={
+                            <>
+                                <Text align="center">
+                                    Profile
+                                </Text>
+                            </>
+                         } 
+                         labelPosition="center"
+                          size="lg" />
+
+                <Grid columns={3} mt="md">  {/* coupon to be shown speratly from its document,to show the value not the id  */}
+                    {userMapped?.map((item: any) => {
+                        return (
+                            <React.Fragment key={item.key}>
+                                <Grid.Col span={1}>
+                                    <div>{item.key} :</div>
+                                </Grid.Col>
+                                <Grid.Col span={2}>
+                                    <Text >
+                                        <div>{item.value.toString()}</div>
+                                    </Text>
+                                </Grid.Col>
+                            </React.Fragment>
+                        )
+                    }
+                    )
+                    }
+                    {!user.coupon &&  // to be done later 
+                        <>
                             <Grid.Col span={1}>
-                                <div>{key} :</div>
+                                <div>coupon :</div>
                             </Grid.Col>
                             <Grid.Col span={2}>
 
                                 <Text >
-                                    <div>{value.toString()}</div>
+                                    <div> </div>
                                 </Text>
+
                             </Grid.Col>
-                        </React.Fragment>
-                    )
-                }
-                )
-                }
-                {!user.coupon &&
-                    <>
-                        <Grid.Col span={1}>
-                            <div>coupon :</div>
-                        </Grid.Col>
-                        <Grid.Col span={2}>
+                        </>
+                    }
 
-                            <Text >
-                                <div> </div>
-                            </Text>
-
-                        </Grid.Col>
-                    </>
-                }
-
-            </Grid>
-        </>
-    )
+                </Grid>
+            </>
+        )
 }
 
 export default Profile
