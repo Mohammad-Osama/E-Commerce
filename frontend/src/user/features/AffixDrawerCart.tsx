ce205cc9cb66d8a1fc59abebe3e68d66bc565003
Mohammad <moh.osama84x@gmail.com>
2022-09-07 22:15:50 +0200

    F - removefromcart button in affix,other buttons

--- frontend/src/user/features/AffixDrawerCart.tsx
+++ frontend/src/user/features/AffixDrawerCart.tsx
@@ -1,8 +1,10 @@
-import { ShoppingCart } from 'tabler-icons-react';
-import { Affix, Group, Space, ScrollArea, Avatar, Badge, Button, Text, Transition, ActionIcon, Drawer, SimpleGrid, Divider } from '@mantine/core';
+import { ShoppingCart, ShoppingCartX, ShoppingCartOff } from 'tabler-icons-react';
+import { Affix, Tooltip, Group, Space, ScrollArea, Avatar, Badge, Button, Text, Transition, ActionIcon, Drawer, SimpleGrid, Divider } from '@mantine/core';
 import React, { useState, useEffect } from 'react';
-import { cartState } from '../redux/slices/cartSlice';
-import { useSelector } from 'react-redux';
+import { cartState , emptyAllCart } from '../redux/slices/cartSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCart } from '../redux/slices/cartSlice';
+import { showNotification } from '@mantine/notifications';
 
 
 export default function AffixDrawerCart() {
@@ -11,8 +13,26 @@ export default function AffixDrawerCart() {
   const [mount, setMount] = useState(true);
 
   const cartItems = useSelector(cartState)
-
-         console.log(cartItems)
+  const totalCost = () => {
+    let final = 0
+    cartItems.map((item) => {
+        let itemTotal = 0
+        itemTotal = item.quantity * item.price
+        final = final + itemTotal
+    })
+    return final
+}
+  const dispatch = useDispatch()
+  const removeFromCartFunction = (id: string, name: string): void => {
+    dispatch(removeFromCart(id))
+    showNotification({
+      title: "Cart changed",
+      message: `${name} has been removed from the cart`,
+      color: 'red',
+      icon: <ShoppingCartX />
+    })
+  }
+  console.log(cartItems)
   return (
     <>
 
@@ -20,9 +40,11 @@ export default function AffixDrawerCart() {
         size="lg"
         padding="xl"
         opened={opened}
-        onClose={() => {setOpened(false)
-                       setMount(!mount)}
-                        }
+        onClose={() => {
+          setOpened(false)
+          setMount(!mount)
+        }
+        }
         title="Cart"
       >
         <ScrollArea style={{ height: 500 }} type="auto" offsetScrollbars>
@@ -31,26 +53,43 @@ export default function AffixDrawerCart() {
 
           {cartItems.map((item) => {
             return <React.Fragment key={item.id}>
-              <SimpleGrid cols={3} spacing="sm" >
+              <SimpleGrid cols={4} spacing="sm" >
                 <Avatar src={item.main_image} />
                 <div>
                   <Text>{item.name}</Text>
                 </div>
-                <Badge color="pink" variant="light" size="md" >{item.quantity}</Badge>
+                <Badge color="green" variant="outline" size="md" >{item.quantity}</Badge>
+                <ActionIcon onClick={() => { removeFromCartFunction(item.id, item.name) }}>
+                  <Tooltip placement="center" label="Remove from cart" position="bottom" radius="xl" color="red" >
+                    <ShoppingCartOff size={30} color={'red'} />
+                  </Tooltip>
+                </ActionIcon>
               </SimpleGrid>
               <Space h="xl" />
             </React.Fragment>
           })
           }
+          {cartItems.length > 0 &&
+                <SimpleGrid cols={2} spacing="sm" >
+                    <Button fullWidth color="green">
+                        Checkout({totalCost()}) EGP
+                    </Button>
+                    <Button fullWidth color="red"
+                        onClick={() => { dispatch(emptyAllCart()) }}>
+                        Empty the cart </Button>
+                </SimpleGrid>
+            }
         </ScrollArea>
       </Drawer>
 
       <Affix position={{ top: 100, right: 10 }}>
         <Transition transition="slide-up" mounted={mount}>
           {(transitionStyles) => (
-            <ActionIcon onClick={() =>{ setOpened(true)
-                                        setMount(!mount)}
-                                } >
+            <ActionIcon onClick={() => {
+              setOpened(true)
+              setMount(!mount)
+            }
+            } >
               <ShoppingCart size={30} />
             </ActionIcon>
           )}
