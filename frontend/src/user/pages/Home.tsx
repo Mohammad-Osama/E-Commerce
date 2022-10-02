b4cf12af83e4c04a4bbf01ef55b3c73b2baadc26
Mohammad <moh.osama84x@gmail.com>
2022-10-02 02:41:33 +0200

    F - chnage breakpoints in Home containers

--- frontend/src/user/pages/Home.tsx
+++ frontend/src/user/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Loader, Container, SimpleGrid, Text ,useMantineTheme ,createStyles , Button } from '@mantine/core';
+import { Loader, Container, SimpleGrid, Text, useMantineTheme, createStyles, Button } from '@mantine/core';
 import { useState, useEffect } from "react";
 import * as api from "../../helpers/api"
 import { ICategory, IProduct } from '../../helpers/types';
@@ -9,25 +9,25 @@ import TitleText from '../components/TitleText';
 
 
 const useStyles = createStyles((theme) => ({
-    container: {   
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],   
+    container: {
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.cyan[1],
-            
+
     },
     control: {
         paddingLeft: 50,
         paddingRight: 50,
         fontFamily: `Greycliff CF, ${theme.fontFamily}`,
         fontSize: 22,
-    
+
         [theme.fn.smallerThan('md')]: {
-          width: '100%',
+            width: '100%',
         },
-      },
+    },
 }));
 
 const Home = () => {
-  
+
     const { classes } = useStyles()
     const emptyCategories: ICategory[] = [];
     const emptyProducts: IProduct[] = [];
@@ -41,15 +41,15 @@ const Home = () => {
     const update = async () => {
         await api.getCategories()
             .then((res) => {
-              //  console.log("Categoriesssssss ", res);
+                //  console.log("Categoriesssssss ", res);
                 setCategories(res as ICategory[]);
             });
 
         await api.getProducts()
             .then((res) => {
-              //  console.log("Productssssssss ", res);
+                //  console.log("Productssssssss ", res);
                 setProducts(res as IProduct[]);
-              //  console.log("Productssssssss ", products);
+                //  console.log("Productssssssss ", products);
             });
         SetLoading(false);
     }
@@ -65,27 +65,27 @@ const Home = () => {
     }, [loading]);
 
 
-    if (loading === true || products===[] || categories===[] )
+    if (loading === true || products === [] || categories === [])
 
         return <Loader width="100%"
-                       size="xl"
-                 />
+            size="xl"
+        />
 
     else
 
         return (
             <Container size="xl" my="md" pb="xl" className={classes.container}>
-               
-                <TitleText   title="On Sale"
-                             type="On Sale"
-                             typeId=" "
-                  />
-            
+
+                <TitleText title="On Sale"
+                    type="On Sale"
+                    typeId=" "
+                />
+
                 <SimpleGrid cols={3} spacing="lg"
                     breakpoints={[
-                        { maxWidth: 980, cols: 3, spacing: 'md' },
-                        { maxWidth: 755, cols: 2, spacing: 'sm' },
-                        { maxWidth: 600, cols: 1, spacing: 'sm' },
+                        { maxWidth: 1024, cols: 3, spacing: 'md' },
+                        { maxWidth: 768, cols: 2, spacing: 'sm' },
+                        { maxWidth: 500, cols: 1, spacing: 'sm' },
                     ]} >
 
                     {products.map((product) => {
@@ -96,16 +96,16 @@ const Home = () => {
                     })}
                 </SimpleGrid>
 
-                <TitleText   title="Featured"
-                             type="Featured"
-                             typeId=" "
-                  />
+                <TitleText title="Featured"
+                    type="Featured"
+                    typeId=" "
+                />
 
                 <SimpleGrid cols={3} spacing="lg"
                     breakpoints={[
-                        { maxWidth: 980, cols: 3, spacing: 'md' },
-                        { maxWidth: 755, cols: 2, spacing: 'sm' },
-                        { maxWidth: 600, cols: 1, spacing: 'sm' },
+                        { maxWidth: 1024, cols: 3, spacing: 'md' },
+                        { maxWidth: 768, cols: 2, spacing: 'sm' },
+                        { maxWidth: 500, cols: 1, spacing: 'sm' },
                     ]} >
 
 
@@ -118,16 +118,16 @@ const Home = () => {
                 </SimpleGrid>
                 {categories?.map((x) => (
                     <div key={x.id}>
-                        <TitleText  
-                                     title={x.name}
-                                     type="category"
-                                     typeId={x.id}
-                                          />
+                        <TitleText
+                            title={x.name}
+                            type="category"
+                            typeId={x.id}
+                        />
                         <SimpleGrid cols={3} spacing="lg"
                             breakpoints={[
-                                { maxWidth: 980, cols: 3, spacing: 'md' },
-                                { maxWidth: 755, cols: 2, spacing: 'sm' },
-                                { maxWidth: 600, cols: 1, spacing: 'sm' },
+                                { maxWidth: 1024, cols: 3, spacing: 'md' },
+                                { maxWidth: 768, cols: 2, spacing: 'sm' },
+                                { maxWidth: 500, cols: 1, spacing: 'sm' },
                             ]} >
                             {products?.filter((item) => item.category === x.id)
                                 .slice(0, 6).map((product) => {
