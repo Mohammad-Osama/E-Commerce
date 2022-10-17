6e3daa125e6f74867661e718054cc02b0415885f
Mohammad <moh.osama84x@gmail.com>
2022-10-17 18:03:30 +0200

    getServerSideProps in browse

--- pages/browse/[id]/index.tsx
+++ pages/browse/[id]/index.tsx
@@ -1,60 +1,34 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router';
-import * as api from "../../../helpers/api"
 import { Loader, Container, SimpleGrid } from '@mantine/core';
 import { IProduct } from '../../../helpers/types'
 import Product from '../../../components/Product'
+import {  GetServerSidePropsResult, GetServerSidePropsContext } from 'next';
+import { Product as ProductModel } from '../../../models/productModel';
+import clientPromise from '../../../lib/db';
+import mongoose from "mongoose"
 
 
+const Browse = ({ productsProps, loadingProps }: X) => {
+  const router = useRouter()
+  const { id } = router.query
+  const emptyProducts: IProduct[] = [];
+  const [products, setProducts]: [IProduct[], (category: IProduct[]) => void] = useState(emptyProducts)
+  const [loading, SetLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    setProducts(productsProps)
+    SetLoading(loadingProps)
+  }, [id])
 
 
-const Browse = () => {
-    const router =useRouter()
-    const {id}= router.query
-    type LocationState = { type: string; };
-  //  const { type } = router.query.name as unknown as  LocationState // maybe change it 
-  const { type } = router.query
-   /*  const { id } = useParams()
-    const location = useLocation() */
-
-   // type LocationState = { type: string; };
-    //const { type } = location.state as LocationState
-
-    const emptyProducts: IProduct[] = [];
-    const [products, setProducts]: [IProduct[], (category: IProduct[]) => void] = useState(emptyProducts)
-    const [loading, SetLoading] = useState<boolean>(true)
-
-
-    async function getProductsByCategory(id: string) {
-        const productsData = await api.getProductsByCategory(id)
-        setProducts(productsData)
-        SetLoading(false)
-    }
-
-    async function getProductsByBrand(id: string) {
-        const productsData = await api.getProductsByBrand(id)
-        setProducts(productsData)
-        SetLoading(false)
-    }
-
-
-    useEffect(() => {  // better conditions ?
-        if (type === "category") {
-            getProductsByCategory(id as string)
-        }
-        else {
-            getProductsByBrand(id as string)
-        }
-    }, [id,loading])
-
-
-    if (loading === true)
-    return <Loader  width="100%"
-                    size="xl" />
+  if (loading === true)
+    return <Loader width="100%"
+      size="xl" />
   else
 
     return (
-        <Container>
+      <Container>
         <SimpleGrid cols={3} spacing="lg"
           breakpoints={[
             { maxWidth: 980, cols: 3, spacing: 'md' },
@@ -62,15 +36,75 @@ const Browse = () => {
             { maxWidth: 600, cols: 1, spacing: 'sm' },
           ]} >
 
-            {products.map((product) => {
-                return <Product product={product}
-                                key={product.id}   
-                                />
-                    })
-                    }
+          {products.map((product) => {
+            return <Product product={product}
+              key={product.id}
+            />
+          })
+          }
         </SimpleGrid>
       </Container>
     )
 }
 
 export default Browse
+
+interface X {
+  productsProps: IProduct[]
+  loadingProps: boolean
+}
+export async function getServerSideProps(context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<X>> {
+  await clientPromise()
+  try {
+    const { id, type } = context.query
+    let products: IProduct[] = []
+    if (type === "category") {
+      const ObjectId = mongoose.Types.ObjectId
+      const finalID = new ObjectId(id as string);
+      const resProducts = await ProductModel.find({ category: finalID })
+        .limit(6)
+
+      const dataProducts = resProducts.map((doc) => {
+        const product = doc.toObject()
+        product.category = product.category.toString()
+        product.brand = product.brand.toString()
+        product._id = product._id.toString()
+        product.createdAt = product.createdAt.toString()
+        product.updatedAt = product.updatedAt.toString()
+        return product as IProduct
+      })
+      products = dataProducts
+
+    }
+    else {
+      const ObjectId = mongoose.Types.ObjectId
+      const finalID = new ObjectId(id as string);
+      const resProducts = await ProductModel.find({ brand: finalID })
+        .limit(6)
+
+      const dataProducts = resProducts.map((doc) => {
+        const product = doc.toObject()
+        product.category = product.category.toString()
+        product.brand = product.brand.toString()
+        product._id = product._id.toString()
+        product.createdAt = product.createdAt.toString()
+        product.updatedAt = product.updatedAt.toString()
+        return product as IProduct
+      })
+      products = dataProducts
+    }
+    return {
+      props: {
+        productsProps: products,
+        loadingProps: false
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        productsProps: [],
+        loadingProps: true
+      },
+    }
+  }
+}
