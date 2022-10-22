e8d22bedeeda52aef1181f9fcde894de709f7a1e
Mohammad <moh.osama84x@gmail.com>
2022-10-22 12:01:20 +0200

    fix categories and brands api index

--- pages/api/categories/index.ts
+++ pages/api/categories/index.ts
@@ -6,12 +6,11 @@ import { authJwt, CustomRequest } from "../../../middlewareFunctions/authMiddlew
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
   await clientPromise()
-  await authJwt(req, res)
-  const currentUser = ((req as CustomRequest).user)
-  const role = currentUser.role
-  const code = req.body.code
-
   if (req.method === 'POST') {
+    await authJwt(req, res)
+    const currentUser = ((req as CustomRequest).user)
+    const role = currentUser.role
+    const code = req.body.code
       if (code !== process.env.SECRET_CODE) {
           res.status(400).json("code doesnt match")
       }
